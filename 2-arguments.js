02746b1254d73961ad73cbb7469688ef69dd2211
Gift <odinakagift69@gmail.com>
2025-10-29 04:16:48 -0700

    Fix syntax in 2-arguments.js

--- 2-arguments.js
+++ 2-arguments.js
@@ -1,9 +1,10 @@
+
 const args= process .argv.slice
 
 if (args.length ===0) {
     console.log ('No argument');
 } else if (args.length ===1){
-    console.log(''Argument found);
+    console.log('Argument found');
 } else{
     console.log('Arguments found');
 }
