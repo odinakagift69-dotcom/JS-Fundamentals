c1de3edab5c7f330a7021eeb6912c722e26d5e4e
Gift <odinakagift69@gmail.com>
2025-10-29 03:47:50 -0700

    Add script that prints messages based on arguments

---
+++ 2-arguments.js
@@ -0,0 +1,9 @@
+const args= process .argv.slice
+
+if (args.length ===0) {
+    console.log ('No argument');
+} else if (args.length ===1){
+    console.log(''Argument found);
+} else{
+    console.log('Arguments found');
+}
