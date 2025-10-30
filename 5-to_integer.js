37b9432d7fe82a13479211346d66b409310e67e5
Gift <odinakagift69@gmail.com>
2025-10-30 13:57:06 -0700

    Fix ReferenceError by using process.argv instead of arg

--- 5-to_integer.js
+++ 5-to_integer.js
@@ -1,10 +1,10 @@
 #!/usr/bin/node
 
-const arg=process.argv[2];
-const num =parseInt(argv);
+const arg =process.argv[2];
+const num =parseInt(arg);
 
 if (isNaN(num)){
-    console.log('Not a number');
+    console.log(`Not a number`);
 } else{
-    console.log('My number :${num}');
+    console.log(`My number:${num}`);
 }
